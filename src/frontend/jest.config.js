448d0e608d0cb3f29cfad58a5d3a00927d43699d
Rémi Bezot <bezot.remi@gmail.com>
2024-12-23 15:20:58 +0100

    resolve bugs

--- src/frontend/jest.config.js
+++ src/frontend/jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jsdom", // Utilise jsdom pour simuler un environnement de navigateur
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"], // Configuration supplémentaire après l'environnement de test
   moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy", // Mapper les fichiers CSS, LESS, SCSS, SASS pour éviter les erreurs pendant les tests
   },
+  transform: {
+    "^.+\\.jsx?$": "babel-jest", // Utilise babel-jest pour transformer les fichiers .js et .jsx
+    "^.+\\.tsx?$": "ts-jest", // Si tu utilises TypeScript, pour les fichiers .ts et .tsx
+  },
+  moduleFileExtensions: ["js", "jsx", "json", "node", "ts", "tsx"], // Extensions de fichier prises en charge
 };
